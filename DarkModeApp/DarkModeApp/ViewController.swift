78e1f9ee8ee53d4f93197639bfb3b24a40c6ce0e
nazlibekec <nazlibekec@gmail.com>
2023-02-15 00:48:31 +0300

    Finish Commit

--- DarkModeApp/DarkModeApp/ViewController.swift
+++ DarkModeApp/DarkModeApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        //Kullanıcı telefon modu ne olursa olsun burada seçtiğimiz ile çalıştırır.
+        // overrideUserInterfaceStyle = .dark
+        
+        // Button rengini default:Blue seçtiğimden ilk açıldığında blue ile gelmemesi için aynı işlemi burada yaptırdım.
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        } else {
+            changeButton.tintColor = UIColor.black
+        }
     }
-
-
+    
+    // Uygulama açıkken telefonun dark modu değişmesi ile uygulama modunuda değiştiren fonksiyon.
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+       let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        } else {
+            changeButton.tintColor = UIColor.black
+        }
+    }
 }
 
+// Eğitimde kullanıcı modu ne yaparsa yapsın app in tamamında benim seçtiğim modda çalışmasını istiyorsam "User interface Style" seçip dark/light seçiyorum.
+// Fakat bende çıkmadı. Bu sebeple "Appearance" şeklinde aratıp value ise dark/light olarak ayarlayınca aynı aksiyon aldım.
